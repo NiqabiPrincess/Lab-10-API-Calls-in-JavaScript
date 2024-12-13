1f3c019e3117dae798ad282bc72ff685ea132695
unknown <2002zainabaamir@gmail.com>
2024-12-13 17:56:49 -0500

    added event listner for fetch button

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 //* Getting elements from html
-const fetchDataButton = document.getElementById('fetch');
-const xhrDataButton = document.getElementById('xhr');
-const postDataButton = document.getElementById('post');
-const putDataButton = document.getElementById('put');
+const fetchData = document.getElementById('fetch');
+const xhrData = document.getElementById('xhr');
+const postDat = document.getElementById('post');
+const putData = document.getElementById('put');
 const dataDisplay = document.getElementById('display');
 const form = document.getElementById('data-form');
+
+
+//* Fetch  API Section
+fetchData.addEventListener('click', () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('...There was an Error in Loading, Check Your NetWork...');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        displayData(data);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});

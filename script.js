681e9fa120311ef3041c45c6fe9983c2e9bf22bb
unknown <2002zainabaamir@gmail.com>
2024-12-13 17:52:52 -0500

    got elements from html onto javascipt file

---
+++ script.js
@@ -0,0 +1,7 @@
+//* Getting elements from html
+const fetchDataButton = document.getElementById('fetch');
+const xhrDataButton = document.getElementById('xhr');
+const postDataButton = document.getElementById('post');
+const putDataButton = document.getElementById('put');
+const dataDisplay = document.getElementById('display');
+const form = document.getElementById('data-form');
